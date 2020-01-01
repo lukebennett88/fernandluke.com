92033c05c72cfba4f00cd126f4a05e2afc3f0e38
Luke Bennett <hello@lukebennett.com.au>
2020-01-02 00:12:35 +1100

    Add custom form plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const tw = require("tailwindcss/defaultTheme");
+const customForms = require("@tailwindcss/custom-forms");
 
 module.exports = {
   theme: {
@@ -18,15 +19,35 @@ module.exports = {
       spacing: {
         full: "100%"
       }
-    }
+    },
+    customForms: () => ({
+      default: {
+        textarea: {
+          "&:focus": {
+            boxShadow: undefined,
+            borderColor: undefined
+          }
+        }
+      }
+    })
   },
   variants: {},
   plugins: [
+    customForms,
     function({ addUtilities }) {
       const newUtilities = {
         ".blur": {
           "backdrop-filter": "blur(4px)"
         },
+        ".transition-colors": {
+          "transition-property": "background-color, border-color, color"
+        },
+        ".ease-in": {
+          "transition-timing-function": "cubic-bezier(0.4, 0, 1, 1)"
+        },
+        ".transition-medium": {
+          "transition-duration": "200ms"
+        }
       };
       addUtilities(newUtilities);
     }
