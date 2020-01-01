57720affbe17b4afc4fe7ffc48b9cd64c2e1f6bc
Luke Bennett <hello@lukebennett.com.au>
2020-01-01 15:39:19 +1100

    Add new color and refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,25 @@
+const tw = require("tailwindcss/defaultTheme");
+
 module.exports = {
   theme: {
     extend: {
+      colors: {
+        "transparent-black": "hsla(0, 0%, 0%, 0.5)"
+      },
       fontFamily: {
-        sans: [
-          `Helvetica Neue`,
-          `-apple-system`,
-          `BlinkMacSystemFont`,
-          `Segoe UI`,
-          `Roboto`,
-          `Arial`,
-          `Noto Sans`,
-          `sans-serif`,
-          `Apple Color Emoji`,
-          `Segoe UI Emoji`,
-          `Segoe UI Symbol`,
-          `Noto Color Emoji`,
-        ],
-        serif: [
-          `Pistilli`,
-          `Georgia`,
-          `Cambria`,
-          `Times New Roman`,
-          `Times`,
-          `serif`,
-        ],
+        sans: [`Helvetica Neue`, ...tw.fontFamily.sans],
+        serif: [`Pistilli`, ...tw.fontFamily.serif]
       },
       fontSize: {
-        '7xl': '5rem',
-        '8xl': '6rem',
-        '9xl': '7rem',
+        "7xl": "5rem",
+        "8xl": "6rem",
+        "9xl": "7rem"
       },
       spacing: {
-        full: '100%',
-      },
-    },
+        full: "100%"
+      }
+    }
   },
   variants: {},
-  plugins: [],
+  plugins: []
 };
