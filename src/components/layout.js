60c08cf774f1a3a9f6ec114ee2126a2d63264901
Luke Bennett <hello@lukebennett.com.au>
2020-01-02 00:13:22 +1100

    Add transitions and formatting

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,11 +5,11 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useStaticQuery, graphql } from 'gatsby';
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
 
-import Header from './header';
+import Header from "./header";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -23,14 +23,14 @@ const Layout = ({ children }) => {
   `);
 
   return (
-    <div className="flex flex-col min-h-screen font-sans antialiased text-gray-600">
+    <div className="flex flex-col min-h-screen font-sans antialiased text-gray-600 transition-colors ease-in transition-medium">
       <Header />
       <main id="main" className="flex-1 w-full max-w-4xl px-4 pb-6 mx-auto">
         {children}
       </main>
       <footer className="text-center">
         <div className="w-full max-w-4xl p-4 mx-auto">
-          For all wedding related enquires, please contact us via{' '}
+          For all wedding related enquires, please contact us via{" "}
           <a
             className="font-bold text-teal-600 hover:underline"
             href="mailto:hello@fernandluke.com?subject=%23flukefest"
@@ -44,7 +44,7 @@ const Layout = ({ children }) => {
 };
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 };
 
 export default Layout;
