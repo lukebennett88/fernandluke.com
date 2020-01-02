4876e69bb28e966bfd88910b7014176e1d551f3f
Luke Bennett <hello@lukebennett.com.au>
2020-01-02 14:27:36 +1100

    Move Nav Links into their own file

---
+++ src/components/nav-links.js
@@ -0,0 +1,29 @@
+const navLinks = [
+  {
+    id: 0,
+    title: 'Home',
+    href: '/',
+  },
+  {
+    id: 1,
+    title: 'RSVP',
+    href: '/rsvp',
+  },
+  {
+    id: 2,
+    title: 'Venue',
+    href: '#venue',
+  },
+  {
+    id: 3,
+    title: 'Accommodation',
+    href: '#accommodation',
+  },
+  {
+    id: 4,
+    title: 'Gifts',
+    href: '#gifts',
+  },
+];
+
+export default navLinks;
